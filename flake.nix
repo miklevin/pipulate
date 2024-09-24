38dbd52a694ba00ce1f6725f2ada61a16c7fb3aa
Mike Levin <miklevin@gmail.com>
2024-09-24 13:12:12 -0400

    Made to work on mac

--- flake.nix
+++ flake.nix
@@ -12,41 +12,49 @@
       localConfig = if builtins.pathExists ./local.nix then import ./local.nix else {};
       cudaSupport = if localConfig ? cudaSupport then localConfig.cudaSupport else false;
       
-      cudaPackages = pkgs.lib.optionals (cudaSupport && system == "x86_64-linux") (with pkgs; [
-        cudatoolkit
-        cudnn
-        (ollama.override { acceleration = "cuda"; })
-      ]);
+      isLinux = pkgs.stdenv.isLinux;
+      isDarwin = pkgs.stdenv.isDarwin;
       
-      envWithScript = script:
-        (pkgs.buildFHSUserEnv {
-          name = "python-env";
-          targetPkgs = pkgs: (with pkgs; [
-            python311
-            python311.pkgs.pip
-            python311.pkgs.virtualenv
-            pythonManylinuxPackages.manylinux2014Package
-            cmake
-            ninja
-            gcc
-            git
-            stdenv.cc.cc.lib
-          ] ++ cudaPackages);
-          runScript = "${pkgs.writeShellScriptBin "runScript" (''
-            set -e
-            export NIXPKGS_ALLOW_UNFREE=1
-            export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH
-            echo "Welcome to the Pipulate development environment on ${system}!"
-            ${if cudaSupport then "echo 'CUDA support enabled.'" else ""}
-            test -d .venv || ${pkgs.python311.interpreter} -m venv .venv
-            source .venv/bin/activate
-            pip install --upgrade pip
-            pip install -r requirements.txt
-            ${script}
-          '')}/bin/runScript";
-        })
-        .env;
+      commonPackages = with pkgs; [
+        python311
+        python311.pkgs.pip
+        python311.pkgs.virtualenv
+        cmake
+        ninja
+        gcc
+        git
+      ];
+      
+      runScript = pkgs.writeShellScriptBin "runScript" ''
+        set -e
+        export NIXPKGS_ALLOW_UNFREE=1
+        ${if isLinux then "export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH" else ""}
+        echo "Welcome to the Pipulate development environment on ${system}!"
+        ${if cudaSupport && isLinux then "echo 'CUDA support enabled.'" else ""}
+        test -d .venv || ${pkgs.python311.interpreter} -m venv .venv
+        source .venv/bin/activate
+        pip install --upgrade pip
+        pip install -r requirements.txt
+        exec bash
+      '';
+      
+      linuxDevShell = pkgs.mkShell {
+        buildInputs = commonPackages ++ (with pkgs; [
+          pythonManylinuxPackages.manylinux2014Package
+          stdenv.cc.cc.lib
+        ]) ++ pkgs.lib.optionals (cudaSupport && system == "x86_64-linux") (with pkgs; [
+          cudatoolkit
+          cudnn
+          (ollama.override { acceleration = "cuda"; })
+        ]);
+        shellHook = "${runScript}/bin/runScript";
+      };
+      
+      darwinDevShell = pkgs.mkShell {
+        buildInputs = commonPackages;
+        shellHook = "${runScript}/bin/runScript";
+      };
     in {
-      devShell = envWithScript "bash";
+      devShell = if isLinux then linuxDevShell else darwinDevShell;
     });
 }
