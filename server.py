e67adad3ebdc5bb72745497b27c854893544f2ef
Mike Levin <miklevin@gmail.com>
2024-10-03 19:26:39 -0400

    Simulating long-running background task with fasthtml

--- server.py
+++ server.py
@@ -1,35 +1,43 @@
 from fasthtml.common import *
+import asyncio
 
-app = FastHTML(ws_hdr=True)  # Enable WebSocket header
+app = FastHTML(ws_hdr=True)
 rt = app.route
 
-# Home route
+msgs = []
 @rt('/')
-def home():
-    cts = Div(
-        Div(id='notifications'),  # Div to display notifications
-        Form(Input(id='msg'), id='form', ws_send=True),  # Form to send messages
-        hx_ext='ws',  # Enable HTMX WebSocket extension
-        ws_connect='/ws'  # Connect to the WebSocket endpoint
-    )
-    return Titled('WebSocket Test', cts)
+def home(): return Div(
+    Div(Ul(*[Li(m) for m in msgs], id='msg-list')),
+    Form(Input(id='msg', name='msg', placeholder='Type a message...'),  # Input for new messages
+         Button("Send", type='submit', ws_send=True)),  # Button to send message
+    hx_ext='ws',  # Enable HTMX WebSocket extension
+    ws_connect='/ws'  # Connect to the WebSocket endpoint
+)
 
-# WebSocket connection management
 users = {}
+def on_conn(ws, send): users[str(id(ws))] = send
+def on_disconn(ws): users.pop(str(id(ws)), None)
 
-def on_conn(ws, send):
-    users[str(id(ws))] = send  # Store the send function for the connected user
-
-def on_disconn(ws):
-    users.pop(str(id(ws)), None)  # Remove the user on disconnect
-
-# WebSocket route
 @app.ws('/ws', conn=on_conn, disconn=on_disconn)
-async def ws(msg: str, send):
-    await send(Div('Hello ' + msg, id='notifications'))  # Send a greeting message
-    # Optionally, you can broadcast the message to all connected users
-    for u in users.values():
-        await u(Div('User said: ' + msg, id='notifications'))
+async def ws(msg: str):
+    if msg:  # Check if the message is not empty
+        msgs.append(msg)  # Append the message to the list
+        # Simulate a long-running task
+        await long_running_task(msg)  # Call the long-running task
+        # Update all users with the new message list
+        for u in users.values():
+            await u(Ul(*[Li(m) for m in msgs], id='msg-list'))  # Update the message list for each user
 
-# Start the server
+async def long_running_task(msg: str):
+    # Simulate a long-running task
+    for i in range(5):
+        await asyncio.sleep(1)  # Simulate processing time
+        # Send progress updates to all users
+        for u in users.values():
+            await u(Div(f"Processing '{msg}'... Step {i + 1}/5", id='msg-list'))
+
+    # Final update after the task is complete
+    for u in users.values():
+        await u(Div(f"Task complete for message: '{msg}'", id='msg-list'))
+
 serve()
