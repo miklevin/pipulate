42df0a928aef72fc355440879a99fa29b096e078
Mike Levin <miklevin@gmail.com>
2024-10-04 17:16:31 -0400

    Todo app complete

--- todo_app.py
+++ todo_app.py
@@ -1,19 +1,50 @@
 from fasthtml.common import *
 
-def render(todo):
-    return Li(todo.title)
 
-app, rt, todos, Todo = fast_app("todo.db", live=True, render=render,
+def render(todo):
+    tid = f'todo-{todo.id}'
+    toggle = A('Toggle', hx_get=f'/toggle/{todo.id}', target_id=tid)
+    delete = A('Delete', hx_delete=f'/{todo.id}', 
+               hx_swap='outerHTML', target_id=tid)
+    return Li(toggle, ' | ', delete, ' ',
+              todo.title + (' (Done)' if todo.done else ''),
+              id=tid)
+
+
+app, rt, todos, Todo = fast_app("data/todo.db", live=True, render=render,
                                 id=int, title=str, done=bool, pk="id")
 
-@rt("/")
+
+def mk_input(): return Input(placeholder='Add a new item', 
+                             id='title', hx_swap_oob='true')
+
+
+@rt('/')
 def get():
-
-    todos.insert(Todo(title='Todo per page-load', done=False))
-
+    frm = Form(Group(mk_input(),
+               Button('Add')),
+               hx_post='/', target_id='todo-list', hx_swap="beforeend")
     return Titled('Todos', 
-                  Ul(*todos()),
+                  Card(
+                  Ul(*todos(), id='todo-list'),
+                  header=frm)
                 )
+
+
+@rt('/')
+def post(todo:Todo): return todos.insert(todo), mk_input()
+
+
+@rt('/{tid}')
+def delete(tid:int): todos.delete(tid)
+    
+
+@rt('/toggle/{tid}')
+def get(tid:int):
+    todo = todos[tid]
+    todo.done = not todo.done
+    return todos.update(todo)
+
 
 serve()
 
