28cad5cdf608c57a28e63a0599bcb92d711b6620
Mike Levin <miklevin@gmail.com>
2025-10-07 13:02:50 -0400

    fix: Update version and description strings
    Fixing version string and description string to reflect Mac install fix.

--- __init__.py
+++ __init__.py
@@ -12,8 +12,8 @@ Usage:
     pipulate
 """
 
-__version__ = "1.2.3"
-__version_description__ = "Working in Google Colab"
+__version__ = "1.2.4"
+__version_description__ = "Fixing Mac Install"
 __email__ = "pipulate@gmail.com"
 __description__ = "Local First AI SEO Software" 
 
