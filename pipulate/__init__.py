6c36d1b3668d522dad4c8fbf78cbab69aec2d2e3
Mike Levin <miklevin@gmail.com>
2025-10-08 15:55:44 -0400

    fix: Update version and description in pipulate/__init__.py
    Updated the version number in `pipulate/__init__.py` from "1.4.1" to "1.4.2" and the version description to "Showing correct version on banner". This ensures consistency across the project's version information.

--- pipulate/__init__.py
+++ pipulate/__init__.py
@@ -6,8 +6,8 @@ from .core import Pipulate
 from .pipulate import pip
 
 # --- START: Version Information (DO NOT REMOVE) ---
-__version__ = "1.2.1"
-__version_description__ = "JupyterLab Integration"
+__version__ = "1.4.2"
+__version_description__ = "Local First AI SEO Software"
 # --- END: Version Information ---
 
 # This allows `from pipulate import Pipulate` and makes the `pip` object (from pipulate.py) available.
