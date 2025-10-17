c61942575ee07095a6e12fba37e21e169ace1972
Mike Levin <miklevin@gmail.com>
2025-10-17 09:12:06 -0400

    feat: Implement Tag 'Em step to identify and score content gaps in GAPalyzer. Also uses 1st item in URL list for client

--- foo_files.py
+++ foo_files.py
@@ -1,6 +1,6 @@
 FILES_TO_INCLUDE_RAW = """\
-/home/mike/repos/pipulate/Notebooks/40_GAP_Analysis.py
-# /home/mike/repos/pipulate/assets/nbs/GAPalyzer.ipynb
+# /home/mike/repos/pipulate/Notebooks/40_GAP_Analysis.py
+/home/mike/repos/pipulate/assets/nbs/GAPalyzer.ipynb
 # /home/mike/repos/pipulate/Notebooks/secretsauce.py
 """
 
