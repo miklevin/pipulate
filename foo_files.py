cf92461c66035c941b1bacb2ab28628fca07f164
Mike Levin <miklevin@gmail.com>
2025-10-18 22:31:39 -0400

    Updated path to the Hello World script in foo_files.py

--- foo_files.py
+++ foo_files.py
@@ -1,9 +1,9 @@
 FILES_TO_INCLUDE_RAW = """\
-# /home/mike/repos/pipulate/Notebooks/40_GAP_Analysis.py
 # /home/mike/repos/pipulate/Notebooks/secretsauce.py
-# /home/mike/repos/pipulate/assets/nbs/FAQuilizer.ipynb
+# /home/mike/repos/pipulate/pipulate/core.py
+/home/mike/repos/pipulate/assets/nbs/FAQuilizer.ipynb
 # /home/mike/repos/pipulate/assets/nbs/GAPalyzer.ipynb
-/home/mike/repos/pipulate/Notebooks/GAPalyzer.ipynb
+# /home/mike/repos/pipulate/assets/nbs/A_Hi_2_AI.ipynb
 """
 
 # /home/mike/repos/pipulate/.gitignore
@@ -41,7 +41,7 @@ FILES_TO_INCLUDE_RAW = """\
 # /home/mike/repos/pipulate/pyproject.toml
 
 # /home/mike/repos/pipulate/Notebooks/AI_Scrape.ipynb
-# /home/mike/repos/pipulate/Notebooks/A_Hi_2_AI.ipynb
+# /home/mike/repos/pipulate/assets/nbs/A_Hi_2_AI.ipynb
 # /home/mike/repos/pipulate/Notebooks/prompt.txt
 # /home/mike/repos/pipulate/tools/__init__.py 
 
@@ -86,7 +86,7 @@ FILES_TO_INCLUDE_RAW = """\
 # /home/mike/repos/pipulate/pipulate/core.py
 # /home/mike/repos/pipulate/pipulate/pipulate.py
 # /home/mike/repos/pipulate/apps/040_hello_workflow.py
-# /home/mike/repos/pipulate/scripts/botify/A_Hi_2_AI.ipynb
+# /home/mike/repos/pipulate/assets/nbs/A_Hi_2_AI.ipynb
 
 # DRY OO APP PLUGINS
 # /home/mike/repos/pipulate/imports/crud.py
@@ -179,7 +179,7 @@ FILES_TO_INCLUDE_RAW = """\
 # /home/mike/repos/pipulate/Notebooks/Client_Work/faquilizer/src/faquilizer/__init__.py
 # /home/mike/repos/pipulate/Notebooks/Client_Work/faquilizer/src/faquilizer/template.py
 # /home/mike/repos/pipulate/Notebooks/Client_Work/faquilizer/AI_Scrape.ipynb
-# /home/mike/repos/pipulate/scripts/botify/A_Hi_2_AI.ipynb
+# /home/mike/repos/pipulate/assets/nbs/A_Hi_2_AI.ipynb
 # /home/mike/repos/pipulate/Notebooks/Client_Work/faquilizer/faq-session-01_output.xlsx
 
 # PREVIOUS CONTEXT DUMPS (PROBABLY DELETE BELOW THIS POINT)
@@ -228,7 +228,7 @@ FILES_TO_INCLUDE_RAW = """\
 # /home/mike/repos/pipulate/pipulate/core.py
 # /home/mike/repos/pipulate/pipulate/pipulate.py
 # /home/mike/repos/pipulate/apps/040_hello_workflow.py
-# /home/mike/repos/pipulate/scripts/botify/A_Hi_2_AI.ipynb
+# /home/mike/repos/pipulate/assets/nbs/A_Hi_2_AI.ipynb
 # /home/mike/repos/pipulate/imports/crud.py
 # /home/mike/repos/pipulate/apps/060_tasks.py
 # /home/mike/repos/pipulate/apps/030_roles.py
@@ -320,7 +320,6 @@ FILES_TO_INCLUDE_RAW = """\
 # /home/mike/repos/pipulate/assets/nbs/A_Hi_2_AI.ipynb
 # /home/mike/repos/pipulate/assets/nbs/secretsauce.py
 # /home/mike/repos/pipulate/assets/nbs/AI_Scrape.ipynb
-# /home/mike/repos/pipulate/Notebooks/A_Hi_2_AI.ipynb
 # /home/mike/repos/pipulate/Notebooks/open_eyes.ipynb
 # /home/mike/repos/pipulate/Notebooks/secretsauce.py
 # /home/mike/repos/pipulate/Notebooks/AI_Scrape.ipynb
