741c2911356cf509614f386236a2934166e5b22f
Mike Levin <miklevin@gmail.com>
2025-10-20 07:27:02 -0400

    Getting ready to restrict eza tree generation in Prompt Fu to only when .py files are present

--- foo_files.py
+++ foo_files.py
@@ -1,5 +1,6 @@
 FILES_TO_INCLUDE_RAW = """\
-# /home/mike/repos/pipulate/flake.nix
+/home/mike/repos/pipulate/README.md
+/home/mike/repos/pipulate/flake.nix
 # /home/mike/repos/pipulate/.gitignore
 # /home/mike/repos/pipulate/pipulate/__init__.py
 # /home/mike/repos/pipulate/pipulate/pipulate.py
@@ -7,7 +8,7 @@ FILES_TO_INCLUDE_RAW = """\
 # /home/mike/repos/pipulate/pipulate/core.py
 # /home/mike/repos/pipulate/Notebooks/A_Hi_2_AI.ipynb
 # /home/mike/repos/pipulate/Notebooks/FAQuilizer.ipynb
-/home/mike/repos/pipulate/Notebooks/GAPalyzer.ipynb
+# /home/mike/repos/pipulate/Notebooks/GAPalyzer.ipynb
 # /home/mike/repos/pipulate/assets/nbs/A_Hi_2_AI.ipynb
 # /home/mike/repos/pipulate/assets/nbs/GAPalyzer.ipynb
 # /home/mike/repos/pipulate/assets/nbs/FAQuilizer.ipynb
