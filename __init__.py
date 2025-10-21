05be224ba3c3d41597ab92e03853e0ae701072f5
Mike Levin <miklevin@gmail.com>
2025-10-21 19:52:51 -0400

    chore: Update __version__ in __init__.py and pyproject.toml
    Update version string to "1.4.5" in both the __init__.py and pyproject.toml files.

--- __init__.py
+++ __init__.py
@@ -12,7 +12,7 @@ Usage:
     pipulate
 """
 
-__version__ = "1.4.4"
+__version__ = "1.4.5"
 __version_description__ = "Silent Stealth Crawling"
 __email__ = "pipulate@gmail.com"
 __description__ = "Local First AI SEO Software" 
