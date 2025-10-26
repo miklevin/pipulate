58d1dcb0ee33876e097b9af4ef1dd5ac3c5abae2
Mike Levin <miklevin@gmail.com>
2025-10-26 11:52:31 -0400

    Final fix to make FAQuilizer work again post AI Ergonomics and Git hygiene refactor

--- foo_files.py
+++ foo_files.py
@@ -1,13 +1,14 @@
 AI_PHOOEY_CHOP = """\
-/home/mike/repos/pipulate/Notebooks/FAQuilizer.ipynb
-/home/mike/repos/pipulate/assets/nbs/FAQuilizer.ipynb
-/home/mike/repos/pipulate/Notebooks/imports/faq_writer_sauce.py
-/home/mike/repos/pipulate/assets/nbs/faq_writer_sauce.py
+/home/mike/repos/pipulate/.gitignore
 /home/mike/repos/pipulate/Notebooks/imports/visualize_dom.py
 /home/mike/repos/pipulate/flake.nix
 /home/mike/repos/pipulate/pipulate/__init__.py
 /home/mike/repos/pipulate/pipulate/pipulate.py
 /home/mike/repos/pipulate/pipulate/core.py
+/home/mike/repos/pipulate/Notebooks/FAQuilizer.ipynb
+/home/mike/repos/pipulate/assets/nbs/FAQuilizer.ipynb
+/home/mike/repos/pipulate/Notebooks/imports/faq_writer_sauce.py
+/home/mike/repos/pipulate/assets/nbs/faq_writer_sauce.py
 """
 
 # HISTORY OF PAYLOADS, IF YOU INSIST
