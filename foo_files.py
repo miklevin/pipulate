f22138b8cd4b9bcf59ed5fbbff44e5b155766a82
Mike Levin <miklevin@gmail.com>
2025-10-27 20:11:42 -0400

    Better Gadget Aesthetics

--- foo_files.py
+++ foo_files.py
@@ -1,8 +1,10 @@
 AI_PHOOEY_CHOP = """\
 /home/mike/repos/pipulate/Notebooks/URLinspector.ipynb
 /home/mike/repos/pipulate/Notebooks/imports/url_inspect_sauce.py
+/home/mike/repos/pipulate/Notebooks/imports/faq_writer_sauce.py
 /home/mike/repos/pipulate/Notebooks/imports/seo_gadget.py
-/home/mike/repos/pipulate/Notebooks/imports/visualize_dom.py
+# /home/mike/repos/pipulate/.gitignore
+/home/mike/repos/pipulate/flake.nix
 """
 
 # HISTORY OF PAYLOADS, IF YOU INSIST
