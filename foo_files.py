af6e493ac6ef164218b6bbca895d62d9b9238ef3
Mike Levin <miklevin@gmail.com>
2025-11-25 00:31:44 -0500

    Final formatting fixes on GAPalyzer

--- foo_files.py
+++ foo_files.py
@@ -1,4 +1,8 @@
 AI_PHOOEY_CHOP = """\
+"""
+
+# HISTORY OF PAYLOADS (BUNDLES OF CONTEXT)
+
 # /home/mike/repos/nixos/configuration.nix.old_big_pile
 # /home/mike/repos/nixos/configuration.nix
 # /home/mike/repos/nixos/packages.nix
@@ -8,12 +12,9 @@ AI_PHOOEY_CHOP = """\
 # /home/mike/repos/pipulate/pipulate/__init__.py
 # /home/mike/repos/pipulate/pipulate/pipulate.py
 # /home/mike/repos/pipulate/pipulate/core.py
-/home/mike/repos/pipulate/Notebooks/_config.py
-/home/mike/repos/pipulate/Notebooks/GAPalyzer.ipynb
-/home/mike/repos/pipulate/Notebooks/imports/gap_analyzer_sauce.py
-"""
-
-# HISTORY OF PAYLOADS (BUNDLES OF CONTEXT)
+# /home/mike/repos/pipulate/Notebooks/_config.py
+# /home/mike/repos/pipulate/Notebooks/GAPalyzer.ipynb
+# /home/mike/repos/pipulate/Notebooks/imports/gap_analyzer_sauce.py
 
 # /home/mike/repos/nixos/configuration.nix.old_big_pile
 # /home/mike/repos/nixos/configuration.nix
