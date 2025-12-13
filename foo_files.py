92e54531a656226268dca6d8cf221c4fcb72f8a2
Mike Levin <miklevin@gmail.com>
2025-12-13 06:31:45 -0500

    foo_files.py becoming more important, so cleaning up

--- foo_files.py
+++ foo_files.py
@@ -1,15 +1,33 @@
 AI_PHOOEY_CHOP = """\
-/home/mike/repos/pipulate/pipulate/__init__.py
-/home/mike/repos/pipulate/pipulate/pipulate.py
-/home/mike/repos/pipulate/pipulate/core.py
-/home/mike/repos/pipulate/imports/voice_synthesis.py
-/home/mike/repos/pipulate/apps/010_introduction.py
 """
 
-# HISTORY OF PAYLOADS (BUNDLES OF CONTEXT)
+# > We've got content, it's groovy context  
+# > Our concatenation just won't stop  
+# > Making stories art for a super-smart  
+# > AI-Phooey chop (Hi-Ya!)
 
+# HISTORY OF PAYLOADS (BUNDLES OF CONTEXT)
+# Sometimes I label the context-bundles for what I was trying to do. Sometimes I don't.
+
+# Showing the AI the "outer" NixOS hosting environment of the Pipulate author
+# /home/mike/repos/nixos/configuration.nix
+# /home/mike/repos/nixos/packages.nix
+# /home/mike/repos/nixos/services.nix
+# /home/mike/repos/nixos/ai-acceleration.nix
+# /home/mike/repos/nixos/init.lua
+# /home/mike/repos/nixos/autognome.py
+
+# Implementing voice interruption
+# /home/mike/repos/pipulate/pipulate/__init__.py
+# /home/mike/repos/pipulate/pipulate/pipulate.py
+# /home/mike/repos/pipulate/pipulate/core.py
+# /home/mike/repos/pipulate/imports/voice_synthesis.py
+# /home/mike/repos/pipulate/apps/010_introduction.py
+
+# Adding voice to homepage
 # /home/mike/repos/pipulate/apps/015_backup_introduction.py
 
+# Working on one of the larger and more complex Notebook workflows still in the sausage factory
 # /home/mike/repos/pipulate/pipulate/__init__.py
 # /home/mike/repos/pipulate/pipulate/pipulate.py
 # /home/mike/repos/pipulate/pipulate/core.py
@@ -18,10 +36,11 @@ AI_PHOOEY_CHOP = """\
 # /home/mike/repos/pipulate/assets/nbs/GAPalyzer.ipynb
 # /home/mike/repos/pipulate/assets/nbs/imports/gap_analyzer_sauce.py
 
+# Extending the articleizer methodology to a per-article summarizer and automatic mermaid diagram inserter
 # /home/mike/repos/pipulate/scripts/articles/contextualizer.py
-
 # /home/mike/repos/pipulate/scripts/articles/diagramizer.py
 
+# Using a selection of articles to check how well I'm doing with contextualizer
 # /home/mike/repos/MikeLev.in/_posts/_context/2025-12-04-forever-machine-digital-sovereignty-ai.json
 # /home/mike/repos/MikeLev.in/_posts/_context/2025-12-04-pipulate-forever-machine-sovereignty-stack.json
 # /home/mike/repos/MikeLev.in/_posts/_context/2025-12-04-the-technomancers-console-pipulate-and-the-art-of-digital-sovereignty.json
@@ -40,6 +59,7 @@ AI_PHOOEY_CHOP = """\
 # /home/mike/repos/MikeLev.in/_posts/2025-12-10-externalizing-notebook-logic-the-chisel-the-sauce-and-ai-context.json
 # /home/mike/repos/MikeLev.in/_posts/2025-12-10-zero-sum-attention-sovereign-tools-gapalyzer-hardening.json
 
+# Giving an example of how articleizer works to set context for AI help in writing contextualizer
 # /home/mike/repos/MikeLev.in/_posts/2025-12-05-pipulate-dayton-moment-context-aware-ai-demo-test-automation.md
 # /home/mike/repos/MikeLev.in/_posts/2025-12-04-pipulate-forever-machine-sovereignty-stack.md
 # /home/mike/repos/MikeLev.in/_posts/2025-12-04-the-technomancers-console-pipulate-and-the-art-of-digital-sovereignty.md
