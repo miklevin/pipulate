6b180a35f16389a4527975fc2ea8c3ffa101fd82
Mike Levin <miklevin@gmail.com>
2026-01-01 11:24:09 -0500

    About to make the report report something

--- foo_files.py
+++ foo_files.py
@@ -10,10 +10,11 @@ AI_PHOOEY_CHOP = """\
 # /home/mike/repos/trimnoir/_layouts/default.html
 # remotes/honeybot/hooks/post-receive
 remotes/honeybot/scripts/logs.py
+remotes/honeybot/scripts/db.py
 remotes/honeybot/scripts/stream.py
 remotes/honeybot/scripts/show.py
 remotes/honeybot/scripts/content_loader.py
-/home/mike/repos/pipulate/remotes/honeybot/scripts/report.py
+remotes/honeybot/scripts/report.py
 """
 
 ## > We've got content, it's groovy context  
