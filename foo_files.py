e13a6d40a5b620b4d3efcee52a421b683ba71263
Mike Levin <miklevin@gmail.com>
2026-01-04 14:09:11 -0500

    Preparting to fix link graph

--- foo_files.py
+++ foo_files.py
@@ -5,15 +5,14 @@ AI_PHOOEY_CHOP = """\
 # /home/mike/repos/trimnoir/_config.yml
 # /home/mike/repos/trimnoir/_layouts/default.html
 # /home/mike/repos/trimnoir/_includes/home_hub.md
-scripts/articles/contextualizer.py
-scripts/articles/generate_semrush_candidates.py
+# scripts/articles/contextualizer.py
+# scripts/articles/generate_semrush_candidates.py
 scripts/articles/gsc_historical_fetch.py
 scripts/articles/gsc_velocity.json
 scripts/articles/build_hierarchy.py
 scripts/articles/build_navgraph.py
 scripts/articles/generate_hubs.py
 scripts/articles/update_graphs.py
-# scripts/articles/graph.json
 scripts/articles/show_graph.html
 """
 
