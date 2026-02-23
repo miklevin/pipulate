c2be41adba7280e4b651ddc9df581c0a32b5b3aa
Mike Levin <miklevin@gmail.com>
2026-02-23 08:19:50 -0500

    Getting started with onboard_sauce.py

--- foo_files.py
+++ foo_files.py
@@ -2,24 +2,24 @@
 # with a vim `Ctrl`+`v` visual selection to edit in and out columns of comment hashes.
 
 AI_PHOOEY_CHOP = """\
-foo_files.py
-prompt_foo.py
+# foo_files.py
+# prompt_foo.py
 
-# # Finally onto the work-at-hand
-# flake.nix
-# pipulate/__init__.py
-# pipulate/pipulate.py
-# pipulate/core.py
-# tools/__init__.py
-# tools/llm_optics.py
-# /home/mike/repos/pipulate/assets/nbs/URLinspector.ipynb
-# /home/mike/repos/pipulate/Notebooks/URLinspector.ipynb
-# /home/mike/repos/pipulate/assets/nbs/imports/url_inspect_sauce.py
-# /home/mike/repos/pipulate/Notebooks/imports/url_inspect_sauce.py
-# /home/mike/repos/pipulate/assets/nbs/0nboard.ipynb
-# /home/mike/repos/pipulate/Notebooks/0nboard.ipynb
-# /home/mike/repos/pipulate/assets/nbs/imports/onboard_sauce.py
-# /home/mike/repos/pipulate/Notebooks/imports/onboard_sauce.py
+# Finally onto the work-at-hand
+flake.nix
+pipulate/__init__.py
+pipulate/pipulate.py
+pipulate/core.py
+tools/__init__.py
+tools/llm_optics.py
+/home/mike/repos/pipulate/assets/nbs/URLinspector.ipynb
+/home/mike/repos/pipulate/Notebooks/URLinspector.ipynb
+/home/mike/repos/pipulate/assets/nbs/imports/url_inspect_sauce.py
+/home/mike/repos/pipulate/Notebooks/imports/url_inspect_sauce.py
+/home/mike/repos/pipulate/assets/nbs/0nboard.ipynb
+/home/mike/repos/pipulate/Notebooks/0nboard.ipynb
+/home/mike/repos/pipulate/assets/nbs/imports/onboard_sauce.py
+/home/mike/repos/pipulate/Notebooks/imports/onboard_sauce.py
 
 # # Catching YOU (yes, you Gemini 3 Fast reading this right now) up on the "now moment".
 # /home/mike/repos/trimnoir/_posts/2026-02-22-stateless-ai-unix-context-engineering.md
