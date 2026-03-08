ab5fa32b3f0fa33e0bc39804d0cbd6ad8c3ea974
Mike Levin <miklevin@gmail.com>
2026-03-08 09:45:51 -0400

    Preparing for the LLMectomy

--- foo_files.py
+++ foo_files.py
@@ -15,20 +15,21 @@
 # 2. foo_files.py  # <-- this
 
 AI_PHOOEY_CHOP = """\
-/home/mike/repos/trimnoir/_posts/2026-03-08-holographic-context-engineering-ai-ready-semantic-maps-web-native-llms.md  # [Idx: 917 | Order: 2 | Tokens: 77,786 | Bytes: 245,940]
+# /home/mike/repos/trimnoir/_posts/2026-03-08-holographic-context-engineering-ai-ready-semantic-maps-web-native-llms.md  # [Idx: 917 | Order: 2 | Tokens: 77,786 | Bytes: 245,940]
+/home/mike/repos/trimnoir/_posts/2025-09-12-git-branch-takeover-promote-feature-branch.md
 
-# Claude Opus 4.6 wrote its own Prompt Fu, so let's let it have a go at this Simon Willison llm thing.
-requirements.in
-requirements.txt
-pipulate/__init__.py
-pipulate/pipulate.py
-pipulate/core.py
-/home/mike/repos/pipulate/assets/nbs/0nboard.ipynb
-/home/mike/repos/pipulate/assets/nbs/imports/onboard_sauce.py
-scripts/articles/diagramizer.py
-assets/nbs/imports/url_inspect_sauce.py
-assets/nbs/imports/faq_writer_sauce.py
-scripts/articles/contextualizer.py
+# # Claude Opus 4.6 wrote its own Prompt Fu, so let's let it have a go at this Simon Willison llm thing.
+# requirements.in
+# requirements.txt
+# pipulate/__init__.py
+# pipulate/pipulate.py
+# pipulate/core.py
+# /home/mike/repos/pipulate/assets/nbs/0nboard.ipynb
+# /home/mike/repos/pipulate/assets/nbs/imports/onboard_sauce.py
+# scripts/articles/diagramizer.py
+# assets/nbs/imports/url_inspect_sauce.py
+# assets/nbs/imports/faq_writer_sauce.py
+# scripts/articles/contextualizer.py
 
 # nixops.sh
 # remotes/honeybot/hooks/post-receive
