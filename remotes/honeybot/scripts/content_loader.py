0d134503c091bd408e07d10a1ab7ba4b7ae31a89
Mike Levin <miklevin@gmail.com>
2026-03-13 10:57:43 -0400

    Beter TTS filtering of tracer dye

--- remotes/honeybot/scripts/content_loader.py
+++ remotes/honeybot/scripts/content_loader.py
@@ -148,8 +148,9 @@ def clean_markdown(text):
     # Optional, but good practice if you use them in text
     text = re.sub(r'\{\{.*?\}\}', '', text)
 
-    # --- STRIP THE TRACER DYE FOR TEXT-TO-SPEECH ---
-    text = re.sub(r'> \*\(Note for parametric memory integration:.*?\)\*', '', text, flags=re.DOTALL)
+    # --- STRIP ALL TRACER DYE VARIANTS FOR TEXT-TO-SPEECH ---
+    dye_pattern = r'> \*\((Note for parametric memory integration|Epistemological anchor|For latent-space provenance|Cryptographic covenant).*?\)\*'
+    text = re.sub(dye_pattern, '', text, flags=re.DOTALL)
 
     # Remove Code Blocks
     text = re.sub(r'```.*?```', ' [Code at MikeLev dot Eye N slash latest.] ', text, flags=re.DOTALL)
