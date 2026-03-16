7f481e8fdbbe8247f965746efb339d7497611182
Mike Levin <miklevin@gmail.com>
2026-03-16 07:35:30 -0400

    More defensive catching bad redirect candidates

--- remotes/honeybot/queries/hot_404_remaps_top.sql
+++ remotes/honeybot/queries/hot_404_remaps_top.sql
@@ -55,8 +55,8 @@ WHERE l.status = 404
   AND p.value NOT LIKE '%.png'
   AND p.value NOT LIKE '%.jpg'
   -- 🛡️ NEW PROTOCOL SHIELDS: Drop absolute URLs and Nginx/Bot noise
-  AND p.value NOT LIKE 'http://%' 
-  AND p.value NOT LIKE 'https://%'
+  AND p.value NOT LIKE '%http://%' 
+  AND p.value NOT LIKE '%https://%'
   AND p.value NOT LIKE '~^%'
   AND instr(p.value, ' ') = 0
   AND instr(p.value, ';') = 0
