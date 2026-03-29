4c2974ebb3d6f18d04b2c9faea53141f4d3ba42c
Mike Levin <miklevin@gmail.com>
2026-03-29 07:27:49 -0400

    SQL ad hoc query 2 of 3 fixed

--- remotes/honeybot/queries/intel_sovereign_bots.sql
+++ remotes/honeybot/queries/intel_sovereign_bots.sql
@@ -1,18 +1,19 @@
 WITH trapdoor_hitters AS (
-    SELECT DISTINCT ip_id, ua_id 
-    FROM daily_logs 
-    WHERE path LIKE '%js_confirm.gif%'
+    SELECT DISTINCT l.ip_id, l.ua_id 
+    FROM daily_logs l
+    JOIN paths p ON l.path_id = p.id
+    WHERE p.value LIKE '%js_confirm.gif%'
 ),
 markdown_negotiators AS (
-    SELECT DISTINCT ip_id, ua_id 
-    FROM daily_logs 
-    WHERE path LIKE '%.md' OR status = 200 -- Adjust if you track the specific content_neg flag in a separate column
+    SELECT DISTINCT t.ip_id, t.ua_id 
+    FROM telemetry t
+    WHERE t.served_md = 1
 )
 SELECT 
-    i.ip_address,
-    u.user_agent_string,
+    i.value as ip_address,
+    u.value as user_agent_string,
     'Apex Predator' as classification
-FROM trapdoor_hitters t
-INNER JOIN markdown_negotiators m ON t.ip_id = m.ip_id
-JOIN ips i ON t.ip_id = i.id
-JOIN user_agents u ON t.ua_id = u.id;
+FROM trapdoor_hitters th
+INNER JOIN markdown_negotiators mn ON th.ip_id = mn.ip_id AND th.ua_id = mn.ua_id
+JOIN ips i ON th.ip_id = i.id
+JOIN user_agents u ON th.ua_id = u.id;
