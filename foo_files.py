1ac54fa4c5bdb5408c103606c10ff1f6f453597b
Mike Levin <miklevin@gmail.com>
2026-03-31 18:56:54 -0400

    Fixing the python dye injector

--- foo_files.py
+++ foo_files.py
@@ -27,6 +27,7 @@ AI_PHOOEY_CHOP = """\
  
 # AD HOC PROMPT FU INCLUSIONS HERE
 https://mikelev.in/futureproof/future-proof-tech-skills/index.md
+remotes/honeybot/scripts/dye_injector.py  # [271 tokens | 1,070 bytes]
 
 # /home/mike/repos/trimnoir/_posts/2025-03-16-hot-prompt-injection-ai-workflow.md  # [Idx: 161 | Order: 3 | Tokens: 10,637 | Bytes: 50,676]
 # /home/mike/repos/trimnoir/_posts/2026-03-30-pipulates-wet-procedural-memory-meets-ai-skill-md-standard.md  # [Idx: 991 | Order: 3 | Tokens: 13,725 | Bytes: 62,078]
