35e8442b71f0b0fe5b30bf54062eec9a35eba77e
Mike Levin <miklevin@gmail.com>
2026-05-07 05:47:51 -0400

    seal alias to clean

--- flake.nix
+++ flake.nix
@@ -633,7 +633,7 @@ runScript = pkgs.writeShellScriptBin "run-script" ''
           alias story='python prompt_foo.py -l [:] --no-tree'
           alias latest='python prompt_foo.py -a [-1:] --no-tree'
           alias release='python release.py --release --force'
-          alias seal='clear && git status'
+          alias clean='clear && git status'
           
           # ---------------------------------------------------------
           # THE SUBSHELL ALIASES (Execute safely from anywhere)
