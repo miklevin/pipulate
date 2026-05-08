dd2666eebe31017d51fb60d6e0b9f88bf19afb79
Mike Levin <miklevin@gmail.com>
2026-05-08 09:45:17 -0400

    Now under AGPLv3, ready for PyPI push

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 """
-Pipulate: Local-First AI SEO Software & Digital Workshop
+Pipulate: A hand-cranked, local-first AI SEO software framework.
 
 Your data. Your AI. Your machine. Your control.
 
@@ -12,23 +12,26 @@ Usage:
     pipulate
 """
 
-__version__ = "1.72"
-__version_description__ = "Ghost Driver Red Light"
+__version__ = "1.73"
+__version_description__ = "Now Under AGPLv3"
 __email__ = "pipulate@gmail.com"
 __description__ = "A Notebook-inspired Hand-cranked Agentic Framework" 
 
-# Pipulate - Local-First AI SEO Software
-# Copyright (C) 2026 Mike Levin
-# 
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-# 
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
+# Pipulate: A hand-cranked, local-first AI SEO software framework.
+# Copyright (C) 2026 Michael Jay Levin
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 # SINGLE SOURCE OF TRUTH FOR VERSION AND DESCRIPTION
 # This version number and description are used across all components:
