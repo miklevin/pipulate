0d01f66e3d952d6766298fcbcc91034eb3a68a25
Mike Levin <miklevin@gmail.com>
2026-05-10 07:56:25 -0400

    Releasing version with Global Voice Toggle into PyPI

--- __init__.py
+++ __init__.py
@@ -12,8 +12,8 @@ Usage:
     pipulate
 """
 
-__version__ = "1.77"
-__version_description__ = "Good Homepage Behavior"
+__version__ = "1.78"
+__version_description__ = "Global Voice Toggle"
 __email__ = "pipulate@gmail.com"
 __description__ = "A Notebook-inspired Hand-cranked Agentic Framework" 
 
