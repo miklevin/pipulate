b832f8d6237e75dd5998d9a2f64795e9823f8f24
Mike Levin <miklevin@gmail.com>
2026-05-14 22:18:43 -0400

    About to add block-selection mode to NeoVim

--- foo_files.py
+++ foo_files.py
@@ -277,6 +277,7 @@ AI_PHOOEY_CHOP = r"""
 # Your daily ebb and flow happens here. Clear this out regularly. Add chapter?
 
 foo_files.py  # [4,028 tokens | 15,548 bytes]       <-- Often included every single time.
+Notebooks/Playground/patch_extractor.ipynb
 prompt_foo.py  # [13,638 tokens | 62,389 bytes]
 
 # !!! AD HOC
