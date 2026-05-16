dc89cd173da4186752a38d66b58c98cdd8769f5b
Mike Levin <miklevin@gmail.com>
2026-05-16 18:49:40 -0400

    About to test Gemini patching ability

--- foo_files.py
+++ foo_files.py
@@ -276,13 +276,14 @@ AI_PHOOEY_CHOP = r"""
 # ============================================================================
 # Your daily ebb and flow happens here. Clear this out regularly. Add chapter?
 
-foo_files.py  # [4,028 tokens | 15,548 bytes]       <-- Often included every single time.
-prompt_foo.py  # [13,638 tokens | 62,389 bytes]
-scripts/articles/lsa.py  # [2,693 tokens | 12,031 bytes]
-apply.py
-patch
+# foo_files.py  # [4,028 tokens | 15,548 bytes]       <-- Often included every single time.
+# prompt_foo.py  # [13,638 tokens | 62,389 bytes]
+# apply.py
+# patch
 
 # !!! AD HOC
+
+scripts/articles/lsa.py  # [2,693 tokens | 12,031 bytes]
 
 # flake.nix
 # requirements.in
