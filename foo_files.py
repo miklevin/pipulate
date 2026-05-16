7637d8f90e04bb715de05480f2af79aa61efef17
Mike Levin <miklevin@gmail.com>
2026-05-16 19:54:32 -0400

    About to tighten the patch rules

--- foo_files.py
+++ foo_files.py
@@ -279,9 +279,9 @@ AI_PHOOEY_CHOP = r"""
 # foo_files.py  # [4,028 tokens | 15,548 bytes]       <-- Often included every single time.
 
 scripts/articles/lsa.py  # [2,693 tokens | 12,031 bytes]
-
-# apply.py
-# prompt_foo.py  # [13,638 tokens | 62,389 bytes]
+prompt_foo.py  # [13,638 tokens | 62,389 bytes]
+patch
+apply.py
 
 # !!! AD HOC
 
