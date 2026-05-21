1881578c2e3d4eee74cb40cc6c857ac98729be94
Mike Levin <miklevin@gmail.com>
2026-05-21 16:44:32 -0400

    chore: Update project file references in Pipulate

--- foo_files.py
+++ foo_files.py
@@ -282,28 +282,28 @@ AI_PHOOEY_CHOP = r"""
 # Your daily ebb and flow happens here. Clear this out regularly. Add chapter?
 
 # CORE STUFF
-logs/server.log  # <-- INCLUDING THIS IS A WHOLE DEBUGGING SYSTEM
-# README.md
+# logs/server.log  # <-- INCLUDING THIS IS A WHOLE DEBUGGING SYSTEM
+README.md
 
 # AS ABOVE
-# /home/mike/repos/Pipulate.com/index.md
-# /home/mike/repos/Pipulate.com/install.sh
-# assets/installer/install.sh  # [2,595 tokens | 10,490 bytes]
+/home/mike/repos/Pipulate.com/index.md
+/home/mike/repos/Pipulate.com/install.sh
+assets/installer/install.sh  # [2,595 tokens | 10,490 bytes]
 
 # IS BELOW OUTER
-# /home/mike/repos/nixos/configuration.nix
-# /home/mike/repos/nixos/packages.nix
-# /home/mike/repos/nixos/autognome.py
-# init.lua
-# scripts/ai.py
+/home/mike/repos/nixos/configuration.nix
+/home/mike/repos/nixos/packages.nix
+/home/mike/repos/nixos/autognome.py
+init.lua
+scripts/ai.py
  
 # BELOW INNER
-# .gitignore
-# flake.nix
-# prompt_foo.py
-# apply.py
-# scripts/xp.py
-# foo_files.py  #  <-- The Prompt Fu router is often included in its own payloads.
+.gitignore
+flake.nix
+prompt_foo.py
+apply.py
+scripts/xp.py
+foo_files.py  #  <-- The Prompt Fu router is often included in its own payloads.
 
 # ARTICLES
 # scripts/articles/lsa.py  # [2,693 tokens | 12,031 bytes]
