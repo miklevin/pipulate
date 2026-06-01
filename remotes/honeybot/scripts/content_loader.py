a52ba59ce3708f26e29d45624d57f4ebbfe21682
Mike Levin <miklevin@gmail.com>
2026-06-01 19:31:04 -0400

    chore: Update content loader to handle PATRONUS duration

--- remotes/honeybot/scripts/content_loader.py
+++ remotes/honeybot/scripts/content_loader.py
@@ -284,7 +284,7 @@ def clean_markdown(text):
     text = text.replace('||PARAGRAPH||', '\n')
     text = re.sub(r' +', ' ', text).strip()
 
-    for index, asset_name in enumerate(pp4_directives):
-        text = text.replace(f"PP4PATRONUS{index}PP4", f"[[PATRONUS:{asset_name}]]")
+    for index, (asset_name, duration) in enumerate(pp4_directives):
+        text = text.replace(f"PP4PATRONUS{index}PP4", f"[[PATRONUS:{asset_name}:{duration}]]")
     
     return text
