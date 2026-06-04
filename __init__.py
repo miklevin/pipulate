fa6a2b1c778f0a03fc48cdab7c0fadeee63d5a00
Mike Levin <miklevin@gmail.com>
2026-06-04 18:39:28 -0400

    ```

--- __init__.py
+++ __init__.py
@@ -12,8 +12,8 @@ Usage:
     pipulate
 """
 
-__version__ = "1.96"
-__version_description__ = "Custom Salting"
+__version__ = "1.97"
+__version_description__ = "Better AI_CONTEXT.md"
 __email__ = "pipulate@gmail.com"
 __description__ = "A Notebook-inspired Hand-cranked Agentic Framework" 
 
