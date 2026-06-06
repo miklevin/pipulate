cf1b18ddb461bd234fa8ff98e2523536a544874a
Mike Levin <miklevin@gmail.com>
2026-06-06 12:10:34 -0400

    chore: Add window and conjure_window functions to flake.nix

--- flake.nix
+++ flake.nix
@@ -546,6 +546,21 @@ print('AI:\n', r.ai)
             local target="''${1:-white_rabbit}"
             "$PIPULATE_ROOT/.venv/bin/python" -c "import os, sys; sys.path.insert(0, os.environ.get('PIPULATE_ROOT', os.getcwd())); from imports.ascii_displays import patronus; target = sys.argv[1] if len(sys.argv) > 1 else 'white_rabbit'; patronus(target)" "$target"
           }
+          window() {
+            local duration="30"
+            if [ "$#" -gt 0 ] && [[ "$1" =~ ^[0-9]+([.][0-9]+)?$ ]]; then
+              duration="$1"
+              shift
+            fi
+            if [ "$#" -eq 0 ]; then
+              echo "Usage: window [duration] command [args...]"
+              return 1
+            fi
+            "$PIPULATE_ROOT/.venv/bin/python" -c "import os, sys; sys.path.insert(0, os.environ.get('PIPULATE_ROOT', os.getcwd())); from imports.ascii_displays import conjure_window; conjure_window(sys.argv[2:], duration=float(sys.argv[1]))" "$duration" "$@"
+          }
+          conjure_window() {
+            window "$@"
+          }
           
           # ---------------------------------------------------------
           # THE SUBSHELL ALIASES (Execute safely from anywhere)
