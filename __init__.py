c9112029815bd388ca314228843213c90735411f
Mike Levin <miklevin@gmail.com>
2026-06-08 16:09:47 -0400

    ```

--- __init__.py
+++ __init__.py
@@ -12,8 +12,8 @@ Usage:
     pipulate
 """
 
-__version__ = "1.98"
-__version_description__ = "Even Better AI_CONTEXT.md"
+__version__ = "1.99"
+__version_description__ = "Softwired Paths"
 __email__ = "pipulate@gmail.com"
 __description__ = "A Notebook-inspired Hand-cranked Agentic Framework" 
 
