a32926684ffee65ef0b8c4d7e36ee19397813d86
Mike Levin <miklevin@gmail.com>
2026-07-18 18:52:32 -0400

    chore: Update task management script

--- apps/060_tasks.py
+++ apps/060_tasks.py
@@ -8,7 +8,6 @@ from server import get_db_filename
 from imports.crud import BaseCrud
 
 ROLES = ['Core']
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
 class PluginIdentityManager:
