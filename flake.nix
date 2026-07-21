b819c5261dff49a3afc84e36466e1284bd6693d2
Mike Levin <miklevin@gmail.com>
2026-07-21 11:16:08 -0400

    chore: Update alias for ahc function in flake.nix

--- flake.nix
+++ flake.nix
@@ -899,7 +899,7 @@ runScript = pkgs.writeShellScriptBin "run-script" ''
           alias foo='(cd ~/repos/pipulate && python prompt_foo.py --no-tree)'
           alias fu='(cd ~/repos/pipulate && python prompt_foo.py)'
           alias default='(cd ~/repos/pipulate && python prompt_foo.py --chop DEFAULT_CHOP --no-tree)'
-          alias ahc='(cd ~/repos/pipulate && python prompt_foo.py --chop ADHOC_CHOP --no-tree)'
+          ahc() { (cd ~/repos/pipulate && python prompt_foo.py --chop ADHOC_CHOP --no-tree "$@"); }
           alias ahe='(cd ~/repos/pipulate && nvim "''${PIPULATE_ADHOC_FILE:-adhoc.txt}")'
           alias pins='(cd ~/repos/pipulate && python prompt_foo.py --chop PINNED_CHOP --no-tree)'
           # THE FIRST WISH: `learn` is to Pipulate what vimtutor is to vim.
