ac23998c99f455d15255c7ca99c65fc8b39ed65a
Mike Levin <miklevin@gmail.com>
2026-07-26 20:53:36 -0400

    chore(router): set AI-edit blast boundary (foo_files.py context curation)

--- foo_files.py
+++ foo_files.py
@@ -51,8 +51,8 @@ AI_PHOOEY_CHOP = r"""                                                           
 # This is a real-time book that's already done and always being written.
 
 # --- START STATS ---
-# There are 1,357 already-written articles about this repo at MikeLev.in (Public)
-# Velocity: 36 published in the last 7 days
+# There are 1,358 already-written articles about this repo at MikeLev.in (Public)
+# Velocity: 37 published in the last 7 days
 # --- END STATS ---
 
 # Most of what's below are relative paths to files in GitHub/pipulate/pipulate
