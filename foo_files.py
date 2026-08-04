78e91978c1ad74bcd2eb89ac37dab1f9d3cc280e
Mike Levin <miklevin@gmail.com>
2026-08-03 20:21:14 -0400

    chore(router): set AI-edit blast boundary (foo_files.py context curation)

--- foo_files.py
+++ foo_files.py
@@ -53,9 +53,9 @@ AI_PHOOEY_CHOP = r"""                                                           
 # --- START STATS ---
 # There are 1,388 already-written articles about this repo at MikeLev.in (Public)
 # Velocity: 29 published in the last 7 days
-# Markdown negotiated: 1,962 reads (0.21% of all responses)
+# Markdown negotiated: 1,987 reads (0.21% of all responses)
 # DOM hydration: 2577 trapdoor triggers from 15 non-local IPs (top-N sample, self excluded)
-# Honeybot telemetry fetched 2026-08-03T16:55Z
+# Honeybot telemetry fetched 2026-08-03T23:42Z
 # --- END STATS ---
 
 # Most of what's below are relative paths to files in GitHub/pipulate/pipulate
