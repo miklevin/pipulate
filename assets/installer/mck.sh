e61befae36a09cc1e4a2edcd3cf3773c62e6870d
Mike Levin <miklevin@gmail.com>
2026-09-05 17:16:52 -0400

    chore: Update Pipulate MCK Bootstrap version to v0.5.0

--- assets/installer/mck.sh
+++ assets/installer/mck.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Pipulate MCK Bootstrap v0.4.0 -- the Mother Cat Kata launcher
+# Pipulate MCK Bootstrap v0.5.0 -- the Mother Cat Kata launcher
 # =============================================================
 #
 # WHAT CHANGED IN v0.4.0 -- A TRAIL MAY CARRY ITS OWN URLS
